e00431e24f25eeb53714830fe1ae595054a9fc54
AAYUSH SHARMA <aayushsharma0909@gmail.com>
2024-02-27 15:54:53 +0530

    logger

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { upload } from "./middlewares/multer.middleware.js";
 import { CORS_ORIGIN } from "./constants.js";
 
 const app = express();
@@ -19,6 +18,15 @@ app.use(
 app.use(express.urlencoded({ extended: true, limit: "64kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  if (req.method === "GET") console.log("\x1b[32m", req.method, req.url);
+  else if (req.method === "POST") console.log("\x1b[33m", req.method, req.url);
+  else if (req.method === "PUT") console.log("\x1b[34m", req.method, req.url);
+  else if (req.method === "DELETE")
+    console.log("\x1b[31m", req.method, req.url);
+  else console.log("\x1b[35m", req.method, req.url);
+  next();
+});
 
 import { userRouter } from "./routes/user.route.js";
 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,10 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/Error.js";
 
 const registerUser = asyncHandler(async (req, res, next) => {
   const { username, email, fullName, password } = req.body;
+  if (!username || !email || !fullName || !password)
+    throw new ApiError(400, "All fields are mandatory!!");
   res.status(200).json({ username, email, fullName, password });
 });
 
